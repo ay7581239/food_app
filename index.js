4f9944332adea0ea0d7e2eee4d0a1a663cca8a08
ay7581239 <ay7581239@gmail.com>
2024-12-28 13:07:02 +0530

    food_item

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express"
+const app=express();
+app.use(express.json())
+
+const PORT=3000;
+let fooddata=[];
+let index=1;
+
+//post
+app.post('/food',(req,res)=>{
+    const {name,price}=req.body
+    const newfood={id:index++,name,price}
+    fooddata.push(newfood)
+    res.status(201).send(newfood)
+})
+// get all data
+app.get('/food',(req,res)=>{
+    res.status(200).send(fooddata);
+})
+
+// get specific id data
+app.get('/food/:id',(req,res)=>{
+    const fooditem=fooddata.find(t=>t.id===parseInt(req.params.id));
+    if(fooditem){
+        res.status(200).send(fooditem);
+    }
+    else
+    res.status(404).send("food not found");
+})
+
+// update food
+app.put('/food/:id',(req,res)=>{
+    const fooditem=fooddata.find(t=>t.id===parseInt(req.params.id));
+    if(fooditem){
+        const {name,price}=req.body;
+        fooditem.name=name;
+        fooditem.price=price;
+        res.status(201).send(fooditem);
+    }
+    else
+    res.status(404).send("food not found");
+})
+
+// delete
+app.delete('/food/:id',(req,res)=>{
+    const ind=fooddata.findIndex(t=>t.id===parseInt(req.params.id));
+    if(ind!=-1){
+        fooddata.splice(ind, 1);
+        res.status(204).send('deleted');
+    }
+    else
+    res.status(404).send("food not found");
+})
+
+app.listen(PORT,()=>{
+    console.log(`server started at port: ${PORT}`);
+})
